cd512468cf8cdb63f6da0aac17879d4a63ab4b46
elizacamber <elizacamber@gmail.com>
2019-08-22 15:29:21 +0200

    app actions

--- app/src/main/java/dev/elizacamber/pettracker/MainActivity.kt
+++ app/src/main/java/dev/elizacamber/pettracker/MainActivity.kt
@@ -1,14 +1,28 @@
 package dev.elizacamber.pettracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.Navigation
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        onNewIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        val action = intent.action
+        val data = intent.dataString
+        if (Intent.ACTION_VIEW == action && data != null) {
+            val destination = data.substring(data.lastIndexOf("/"))
+            if (destination == "/details") {
+                Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_petDetailsFragment)
+            }
+        }
     }
 
 }
--- app/src/main/java/dev/elizacamber/pettracker/ui/main/MainFragment.kt
+++ app/src/main/java/dev/elizacamber/pettracker/ui/main/MainFragment.kt
@@ -1,13 +1,12 @@
 package dev.elizacamber.pettracker.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
-import dev.elizacamber.pettracker.MainActivity
 import dev.elizacamber.pettracker.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
